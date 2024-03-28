1e7e4d6cd33d68233af4570eef491f84dd507acb
salamanzerr <105730669+salamanzerr@users.noreply.github.com>
2024-03-28 13:08:24 -0400

    Create display.php file

---
+++ display.php
@@ -0,0 +1,20 @@
+<?php
+// pull words from user forms
+// output within the story
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="style.css">
+    <title>Tip Calculator</title>
+  </head>
+
+  <body>
+        <footer>Copyright &copy; 2024 Alec Manzer, etc.</footer>
+  </body>
+
+  
