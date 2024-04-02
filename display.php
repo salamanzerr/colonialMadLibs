a39c9601a63f5d46149ae9a38dd6de31a93c5151
Kennedy Greene <94080935+3Greene@users.noreply.github.com>
2024-04-01 21:18:11 -0400

    Its now functional
    Real

--- display.php
+++ display.php
@@ -9,17 +9,12 @@
    $noun6 = $_POST['noun6'];
    $person = $_POST['person'];
    $object = $_POST['object'];
-   $adjectivee = $_POST['adjective'];
+   $adjective = $_POST['adjective'];
    $pet = $_POST['pet'];
-   $battlecry = $_POST['battlecry'];
+   $battleCry = $_POST['battleCry'];
    $weapon = $_POST['weapon'];
    $verb = $_POST['verb'];
    $emotion = $_POST['emotion'];
-
-
-
-
-
 ?>
 
 <!-- start of html -->
@@ -43,40 +38,46 @@
    </div>
 
    <h2>The Story</h2>
-   
-    <!-- start of PHP -->
-    <?php
 
-      // pulling words from HTML forms
-      if ($_SERVER["REQUEST_METHOD"] == "POST") {
+   <div class="main">
+
+      <!-- start of PHP -->
+      <?php
+
+         // pulling words from HTML forms
+         if ($_SERVER["REQUEST_METHOD"] == "POST") {
          $noun = $_POST['noun1'];
 
-      // variable to hold the full story including variables; could make more if we want more options 
-      /*$story = "Own a $noun1 for home defense, since that's what the 
+         // variable to hold the full story including variables; could make more if we want more options 
+         $story = "Own a $noun1 for home defense, since that's what the 
                founding fathers intended. Four ruffians break into my 
-               house. "What the $person?" As I grab my (noun) and Kentucky 
-               (noun). Blow a (object) sized hole through the first man, 
-               he's (adjective) on the spot. Draw my (noun) on the second 
+               house. \"What the $person?\" As I grab my $noun2 and Kentucky 
+               $noun3. Blow a $object sized hole through the first man, 
+               he's $adjective on the spot. Draw my $noun4 on the second 
                man, miss him entirely because it's smoothbore and nails the 
-               neighbors (pet). I have to resort to the (noun) mounted at the 
-               top of the stairs loaded with grape shot, "(battle cry)" 
-               the grape shot shreds two men in the blast, the sound and 
-               extra shrapnel set off car alarms. Fix (weapon) and charge the 
-               last (emotion) rapscallion. He (verb) waiting on the police 
-               to arrive since (noun) wounds are impossible to stitch up. 
-               Just as the founding fathers intended."; */
+               neighbors $pet. I have to resort to the $noun5 mounted at the 
+               top of the stairs loaded with grape shot, \"$battleCry\"! 
+               The grape shot shreds two men in the blast, the sound and 
+               extra shrapnel set off car alarms. Fix $weapon and charge the 
+               last $emotion rapscallion. He $verb waiting on the police 
+               to arrive since $noun6 wounds are impossible to stitch up. 
+               Just as the founding fathers intended.";
 
-      // display story on page
-      echo "<p>Story: $story</p>";
-      } 
+         // display story on page
+         echo "<p>$story</p>";
+         } 
    
-      // make sure the page won't try to process form data until it is submitted
-      else {
-        echo "<p>No data submitted</p>";
-      }
+         // make sure the page won't try to process form data until it is submitted
+         else {
+            echo "<p>No data submitted</p>";
+         }
       ?>
+   </div>
 
-      <footer>&copy; Copyright 2024 Alec Manzer, Sam Zito, Fernando Moran, Janine Beall, Kennedy Greene</footer>
+   <footer>
+      &copy; Copyright 2024 Alec Manzer, Sam Zito, 
+      Fernando Moran, Janine Beall, Kennedy Greene
+   </footer>
 </body>
 </html>
 
